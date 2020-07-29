aed2830f689850e6ccec71bac29e7034ae2f6c0b
WhiteJang <galaxy__12@naver.com>
2020-07-29 15:34:08 +0900

    In LikeLion Class

---
+++ dreamaryproject/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+import page.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', page.views.home, name='home'),
+]
---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\yeong\documents\하얀\멋사\유니라이온\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
