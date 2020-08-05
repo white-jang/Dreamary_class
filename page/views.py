e9b49db44e729440f479a314b16fc3d8ac551491
WhiteJang <galaxy__12@naver.com>
2020-08-05 16:18:04 +0900

    CRUD & Requirements.txt added

--- page/views.py
+++ page/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Designer
 
 # Create your views here.
@@ -12,3 +12,41 @@ def introduce(request):
 def detail(request, designer_id):
     designer = get_object_or_404(Designer, pk=designer_id)
     return render(request, 'detail.html', {'designer' : designer})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    if request.method == 'POST':
+        post = Designer()
+        if 'image' in request.FILES:
+            post.image = request.FILES['image']
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+
+        return redirect('detail', post.id)
+
+def update(request, designer_id):
+    post = get_object_or_404(Designer, pk=designer_id)
+    if request.method == 'POST':
+        post = Designer()
+        if 'image' in request.FILES:
+            post.image = request.FILES['image']
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+
+        return redirect('detail', post.id)
+    else:
+        return render(request, 'update.html', {'designer' : post})
+
+def delete(request, designer_id):
+    post = get_object_or_404(Designer, pk=designer_id)
+    post.delete()
+
+    return redirect('home')
